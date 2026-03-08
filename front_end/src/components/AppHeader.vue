448b0cc18343bc2ad0020353830ca4eac62784b9
Elian Felix <efelix.v@gmail.com>
2026-03-08 19:52:14 -0400

    Update style of main menu

--- front_end/src/components/AppHeader.vue
+++ front_end/src/components/AppHeader.vue
@@ -1,74 +1,76 @@
 <template>
-  <div class="d-flex ga-3 position-absolute top-0 right-0 justify-center align-center my-5"
+  <div class="position-relative">
+    <div class="position-fixed"
        id="mainMenu"
-       tabindex="1"
-  >
-    <v-sheet class="d-flex flex-column pa-2 position-sticky elevaltion-2 justify-space-between"
-             :class="{ 'bg-transparent': !openMenu, 'open-menu': openMenu, 'closed-menu': !openMenu }"
-             rounded
+       tabindex="0"
     >
-      <div class="d-flex ga-2 justify-space-between">
-        <v-btn v-if="!openMenu" icon="mdi-account-circle"></v-btn>
-        <v-btn v-if="!openMenu" :icon="activeThemeIcon" @click="changeActiveTheme"></v-btn>
-        <v-btn :icon="openMenu ? 'mdi-close' : 'mdi-menu'" @click="toggleMenu"></v-btn>
-      </div>
-      <div class="flex-column ga-1 menu-section ma-5 mt-0" :class="openMenu ? 'd-flex' : 'd-none'">
-        <div v-if="showSettings" class="d-flex flex-column ga-1 mx-4 mb-1 menu-item">
-          <div class="menu-sub-heading">Settings</div>
-          <div class="d-flex ga-2 justify-start">
-            <v-btn :disabled="fontSize == 3" size="small" icon="mdi-plus-circle" @click="appStore.incrementFontSize"></v-btn>
-            <v-btn :disabled="fontSize == 1" size="small" icon="mdi-minus-circle" @click="appStore.decrementFontSize"></v-btn>
-            <v-btn size="small" icon="mdi-restore" @click="appStore.startGame"></v-btn>
-            <v-btn size="small" icon="mdi-stop" @click="appStore.endActivity"></v-btn>
-          </div>
-          <div class="mx-auto">
-            <v-number-input
-              v-model="timeInput"
-              persistent-placeholder
-              placeholder="secs"
-              :min="15" :max="180"
-              width="150"
-              density="compact"
-              variant="solo-filled"
-              control-variant="split"
-              hide-details
-              :step="15"
-              @update:model-value="appStore.startGame(timeInput)"
-            ></v-number-input>
-            <!--<v-btn-group border density="compact" variant="tonal">
-              <v-btn icon="mdi-minus"></v-btn>
-              <v-text-field hide-details></v-text-field>
-              <v-btn icon="mdi-plus"></v-btn>
-              <v-btn icon="mdi-check"></v-btn>
-            </v-btn-group> -->
-            <!-- <v-btn size="small" icon="mdi-check"></v-btn> -->
-          </div>
+      <v-sheet class="d-flex flex-column pa-2 justify-space-between"
+               :class="{ 'bg-transparent': !openMenu, 'open-menu': openMenu, 'closed-menu': !openMenu }"
+               rounded
+      >
+        <div class="d-flex ga-2 justify-space-between">
+          <v-btn v-if="!openMenu" icon="mdi-account-circle"></v-btn>
+          <v-btn v-if="!openMenu" :icon="activeThemeIcon" @click="changeActiveTheme"></v-btn>
+          <v-btn :icon="openMenu ? 'mdi-close' : 'mdi-menu'" @click="toggleMenu"></v-btn>
         </div>
-        <div class="menu-item mx-4">
-          <div class="menu-sub-heading">Theme</div>
-          <div class="ml-4">
-            <v-switch v-model="activeTheme"
-                      @update:modelValue="() => {theme.change(activeTheme)}"
-                      false-value="light"
-                      :false-icon="activeThemeIcon"
-                      true-value="dark"
-                      :true-icon="activeThemeIcon"
-                      density="compact" hide-details
-                      color="secondary"
-            >
-              <template v-slot:label>
-                {{ activeTheme }}
-              </template>
-            </v-switch>
+        <div class="flex-column ga-1 menu-section ma-5 mt-0" :class="openMenu ? 'd-flex' : 'd-none'">
+          <div v-if="showSettings" class="d-flex flex-column ga-1 mx-4 mb-1 menu-item">
+            <div class="menu-sub-heading">Settings</div>
+            <div class="d-flex ga-2 justify-start">
+              <v-btn :disabled="fontSize == 3" size="small" icon="mdi-plus-circle" @click="appStore.incrementFontSize"></v-btn>
+              <v-btn :disabled="fontSize == 1" size="small" icon="mdi-minus-circle" @click="appStore.decrementFontSize"></v-btn>
+              <v-btn size="small" icon="mdi-restore" @click="appStore.startGame()"></v-btn>
+              <v-btn size="small" icon="mdi-stop" @click="appStore.endActivity"></v-btn>
+            </div>
+            <div class="mx-auto">
+              <v-number-input
+                v-model="timeInput"
+                persistent-placeholder
+                placeholder="secs"
+                :min="15" :max="180"
+                width="150"
+                density="compact"
+                variant="solo-filled"
+                control-variant="split"
+                hide-details
+                :step="15"
+                @update:model-value="appStore.startGame(timeInput)"
+              ></v-number-input>
+              <!--<v-btn-group border density="compact" variant="tonal">
+                <v-btn icon="mdi-minus"></v-btn>
+                <v-text-field hide-details></v-text-field>
+                <v-btn icon="mdi-plus"></v-btn>
+                <v-btn icon="mdi-check"></v-btn>
+              </v-btn-group> -->
+              <!-- <v-btn size="small" icon="mdi-check"></v-btn> -->
+            </div>
           </div>
+          <div class="menu-item mx-4">
+            <div class="menu-sub-heading">Theme</div>
+            <div class="ml-4">
+              <v-switch v-model="activeTheme"
+                        @update:modelValue="() => {theme.change(activeTheme)}"
+                        false-value="light"
+                        :false-icon="activeThemeIcon"
+                        true-value="dark"
+                        :true-icon="activeThemeIcon"
+                        density="compact" hide-details
+                        color="secondary"
+              >
+                <template v-slot:label>
+                  {{ activeTheme }}
+                </template>
+              </v-switch>
+            </div>
+          </div>
+          <div class="menu-item"><v-list-item to="/">Home</v-list-item></div>
+          <div class="menu-item"><v-list-item to="/board">Board</v-list-item></div>
+          <div class="menu-item"><v-list-item to="/profile">Profile</v-list-item></div>
+          <div class="menu-item"><v-list-item to="/login">Login</v-list-item></div>
+          <div class="menu-item"><v-list-item to="/signup">Signup</v-list-item></div>
         </div>
-        <div class="menu-item"><v-list-item to="/">Home</v-list-item></div>
-        <div class="menu-item"><v-list-item to="/board">Board</v-list-item></div>
-        <div class="menu-item"><v-list-item>Profile</v-list-item></div>
-        <div class="menu-item"><v-list-item to="/login">Login</v-list-item></div>
-        <div class="menu-item"><v-list-item to="/signup">Signup</v-list-item></div>
-      </div>
-    </v-sheet>
+      </v-sheet>
+    </div>
     <div v-if="openMenu" class="menu-backdrop" @click="toggleMenu"></div>
   </div>
 </template>
---
+++ front_end/src/pages/profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex flex-column ga-4 fill-height main-container">
+    <div class="d-flex justify-center align-center bg-red histogram-container">
+      <div>
+        WIP
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>

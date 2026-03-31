849756d26acb7a095b2529be4ab7383dfe935041
Elian Felix <efelix.v@gmail.com>
2026-03-31 00:40:35 -0400

    Add user login integration test, and authorization header to secure routes

--- back_end/testing/fixtures/01_users.sql
+++ back_end/testing/fixtures/01_users.sql
@@ -1,15 +1,15 @@
 INSERT INTO users (user_type, username, passwd_hash, name, email)
 VALUES (
-    'regular',
+    'admin',
     'testivo1',
-    'hashed_password',
+    '$2a$10$7AWR6BsGe.HsAcpi9W7FKeCtZ2USrgwQoVhh8/25Ba3NeyFZ93/E2',
     'testivo',
     'testivo1@mail.test'
 ),
 (
     'regular',
     'testivo2',
-    'hashed_password',
+    '$2a$10$7AWR6BsGe.HsAcpi9W7FKeCtZ2USrgwQoVhh8/25Ba3NeyFZ93/E2',
     'testivo',
     'testivo2@mail.test'
 )
